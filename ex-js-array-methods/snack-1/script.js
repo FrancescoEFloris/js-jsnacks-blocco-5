c53dc3f4118ec1bff8e13ea7446f8e7f8b83202a
Francesco Floris <francescofloris.ef@gmail.com>
2026-04-14 16:16:28 +0200

    snack 1

--- ex-js-array-methods/snack-1/script.js
+++ ex-js-array-methods/snack-1/script.js
@@ -3,3 +3,14 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
+/*
+names.forEach(function(nome) {
+  console.log(nome);
+});
+*/
+
+const nomiConsole = names.map(function (nome) {
+    return nome;
+});
+
+console.log(nomiConsole);
